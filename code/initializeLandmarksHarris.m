a1adc86278e1a50736ca8c6959b5febbb7ee142e
Gioele Zardini <Gioele@public-docking-cx-0032.ethz.ch>
2017-11-29 12:27:42 +0100

    harris changed

--- code/initializeLandmarksHarris.m
+++ code/initializeLandmarksHarris.m
@@ -19,17 +19,17 @@ function [P,X] = initializeLandmarksHarris(kf1,kf2,K,params)
 fprintf('Starting initialization ... \n');
 
 %% step1: extract keypoint-descriptors from first and second keyframe
-P1 = detectHarrisFeatures(kf1,'MinQuality',0.01,'FilterSize',5);
-P2 = detectHarrisFeatures(kf2,'MinQuality',0.01,'FilterSize',5);
+P1 = detectHarrisFeatures(kf1,'MinQuality',params.MinQuality,'FilterSize',params.FilterSize);
+P2 = detectHarrisFeatures(kf2,'MinQuality',params.MinQuality,'FilterSize',params.FilterSize);
 fprintf('\n detected %d keypoints in keyframe 1', length(P1));
 fprintf('\n detected %d keypoints in keyframe 2 \n', length(P2));
-[descriptors1,P1_valid] = extractFeatures(kf1,P1,'BlockSize',11);
-[descriptors2,P2_valid] = extractFeatures(kf2,P2,'BlockSize',11);
+[descriptors1,P1_valid] = extractFeatures(kf1,P1,'BlockSize',params.BlockSize);
+[descriptors2,P2_valid] = extractFeatures(kf2,P2,'BlockSize',params.BlockSize);
 figure('Name','Keypoints Frame 1'); imshow(kf1); hold on; plot(P1); hold off;
 figure('Name','Keypoints Frame 2'); imshow(kf2); hold on; plot(P2); hold off;
 
 %% step2: match keypoints across frames
-indexPairs = matchFeatures(descriptors1,descriptors2,'Unique',true,'MaxRatio',0.8);
+indexPairs = matchFeatures(descriptors1,descriptors2,'Unique',params.Unique,'MaxRatio',params.MaxRatio);
 % select only the matched keypoints
 P1_matched = P1_valid(indexPairs(:,1),:);
 P2_matched = P2_valid(indexPairs(:,1),:);
@@ -37,7 +37,7 @@ fprintf('\n matched %d keypoints across keyframes \n', length(P1_matched));
 
 %% step3: apply RANSAC filter to reject outliers and estimate Fundamental
 [F,inliersIndex] = estimateFundamentalMatrix(P1_matched,P2_matched,...
-    'Method','RANSAC','NumTrials',2000,'DistanceThreshold',0.2);
+    'Method','RANSAC','NumTrials',params.NumTrials,'DistanceThreshold',params.DistanceThreshold);
 % select only matched keypoints that were not rejected by RANSAC
 P1_inliers = P1_matched(inliersIndex);
 P2_inliers = P2_matched(inliersIndex);
